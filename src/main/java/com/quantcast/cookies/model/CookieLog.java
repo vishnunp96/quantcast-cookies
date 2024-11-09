30dafdedbf823390995996582ac766c5217928a0
vnp23 <vnp23@imperial.ac.uk>
2024-11-09 13:44:46 +0530

    Added a new record to hold cookie log data

---
+++ src/main/java/com/quantcast/cookies/model/CookieLog.java
@@ -0,0 +1,6 @@
+package com.quantcast.cookies.model;
+
+import java.time.LocalDateTime;
+
+public record CookieLog(String id, LocalDateTime dateTime) {
+}
--- src/test/java/com/quantcast/cookies/CsvParserTest.java
+++ src/test/java/com/quantcast/cookies/CsvParserTest.java
@@ -4,7 +4,6 @@ import com.quantcast.cookies.parser.CsvParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
