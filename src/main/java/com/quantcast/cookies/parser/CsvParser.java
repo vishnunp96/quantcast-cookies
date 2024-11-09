26a75cf1e647a264d338ea453b8f2403cdb550d2
vnp23 <vnp23@imperial.ac.uk>
2024-11-09 13:43:45 +0530

    Added a CSV parser which take in a CSV file as a string and returns CSV as a list of maps

---
+++ src/main/java/com/quantcast/cookies/parser/CsvParser.java
@@ -0,0 +1,40 @@
+package com.quantcast.cookies.parser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CsvParser {
+
+    private final String delimiter = ",";
+    public List<Map<String, String>> getValues(String csvData) {
+        String[] csvLines = csvData.split("\n");
+        if (csvLines.length < 1) {
+            throw new IllegalArgumentException("Empty csv data");
+        }
+
+        String[] headers = csvLines[0].split(delimiter);
+
+        List<Map<String, String>> csvValues = new ArrayList<>();
+        for (int i = 1; i < csvLines.length; i++) {
+            csvValues.add(getMappedValuesFromRow(headers, csvLines[i]));
+        }
+
+        return csvValues;
+    }
+
+    private Map<String, String> getMappedValuesFromRow(String[] headers, String row)
+            throws IllegalArgumentException {
+        String[] rowValues = row.split(delimiter);
+        if (rowValues.length != headers.length){
+            throw new IllegalArgumentException("CSV row size does not match header size");
+        }
+
+        Map<String, String> mappedValues = new HashMap<>();
+        for (int col = 0; col < rowValues.length; col++) {
+            mappedValues.put(headers[col], rowValues[col]);
+        }
+        return mappedValues;
+    }
+}
---
+++ src/test/java/com/quantcast/cookies/CsvParserTest.java
@@ -0,0 +1,36 @@
+package com.quantcast.cookies;
+
+import com.quantcast.cookies.parser.CsvParser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CsvParserTest {
+
+    CsvParser csvParser = new CsvParser();
+    @Test
+    public void whenGoodCsvIsPassedListOfMapsIsReturned() {
+        String csvData = "cookie,timestamp\nid1,2012\nid2,2024";
+
+        List<Map<String, String>> result = csvParser.getValues(csvData);
+
+        Assertions.assertEquals(2, result.size());
+        Assertions.assertTrue(result.getFirst().containsKey("cookie"));
+        Assertions.assertTrue(result.getFirst().containsKey("timestamp"));
+        Assertions.assertEquals("id2", result.get(1).get("cookie"));
+        Assertions.assertEquals("2024", result.get(1).get("timestamp"));
+    }
+
+    @Test
+    public void whenBadCsvIsPassedErrorIsThrown() {
+        String csvData = "cookie,timestamp\nid1,2012,id2";
+        try {
+            csvParser.getValues(csvData);
+            Assertions.fail("The number of columns in second row do not match the header, should fail.");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+}
